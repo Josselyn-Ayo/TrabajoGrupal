8c8761c2d1aceefa0da1626d7d2244fd91117b34
POO <josselyn.ayoharo@epn.edu.ec>
2025-04-24 09:19:00 -0500

    Ejercicio

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+class Vehiculo{
+    String marca;
+    String modelo;
+    int anio;
+    double velocidad;
+    public Vehiculo(String marca, String modelo, int anio, double velocidad){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.velocidad = velocidad;
+    }
+    public void informacion(){
+        System.out.println("Marca: "+ marca);
+        System.out.println("Modelo: "+ modelo);
+        System.out.println("Año: "+ anio);
+        System.out.println("Velocidad: "+ velocidad +"Km/h");
+    }
+}
+
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
